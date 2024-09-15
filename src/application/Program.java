aff6c110c602c0239725cf108099d72930151bda
Gabriel <gabrielbgwork@gmail.com>
2024-09-15 15:57:25 -0300

    creating class seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(9, "Nelio Pink","nelio@gmail.com", new Date(), 2000.0, obj);
 		
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
