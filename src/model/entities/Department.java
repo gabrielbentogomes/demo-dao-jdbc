04035140540e730eb51b4be864911ad69353143f
Gabriel <gabrielbgwork@gmail.com>
2024-09-15 18:29:52 -0300

    departmentDaoJDBC implemented

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,35 +3,45 @@ package model.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Department implements Serializable{
-	
+public class Department implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
-	private int id;
+
+	private Integer id;
 	private String name;
 	
 	public Department() {
 	}
-	public Department(int id, String name) {
+
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	public int getId() {
+
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -41,14 +51,16 @@ public class Department implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return id == other.id;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
-	
-	
-	
 }
